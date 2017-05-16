304dbe391525f5be9bd94f77c5523ff96f89789b
Rudolf Weiss <rudolf.weiss99@gmail.com>
2017-05-16 15:50:20 +0200

    prvy commit

---
+++ Prvy/Druhy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sk.rudolf.cs
+{
+    class Druhy
+    {
+        static void Main(string[] args)
+        {
+            int sucet = 0;
+            for (int i = 0; i < 11; sucet += i++) Console.WriteLine(i);
+            Console.WriteLine("sucet je " + sucet);
+            float priemer = sucet / 10.0f;
+            Console.WriteLine("priemer je "+priemer);
+            Console.ReadKey();
+        }
+    }
+}
--- Prvy/Program.cs
+++ Prvy/Program.cs
@@ -4,12 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Prvy
+namespace sk.rudolf.cs
 {
     class Program
     {
         static void Main(string[] args)
         {
+            byte Mesiac = byte.Parse(Console.ReadLine());
+            if (Mesiac < 3 || Mesiac == 12)
+            {
+                Console.WriteLine("Zima");
+            }
+            else if (Mesiac < 6) Console.WriteLine("Jar");
+            else if (Mesiac < 9) Console.WriteLine("Leto");
+            else if (Mesiac <= 11) Console.WriteLine("Jesen");
+            else Console.WriteLine("Vela");
+
+            Console.ReadKey();
         }
     }
 }
